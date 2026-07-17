127f94e157b28953f0529e94eec302b37eaafd44
adnan-b-akhlas-dev <adnan.b.akhlas.dev@gmail.com>
2026-07-17 23:28:36 +0600

    fix: cookie store

--- src/app/(auth)/_actions/loginAction.ts
+++ src/app/(auth)/_actions/loginAction.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import env from "@/config/env";
+import { tokenConstant } from "@/constant/tokenConstant";
+import { cookies } from "next/headers";
 
 type TLoginState = {
   success: boolean;
@@ -29,5 +31,20 @@ export const loginAction = async (
   });
 
   const result = await response.json();
+
+  if (result.success) {
+    const cookieStore = await cookies();
+    cookieStore.set(
+      tokenConstant.accessToken,
+      result.data[tokenConstant.accessToken],
+      { httpOnly: true, maxAge: 60 * 60 * 24 * 1, sameSite: "lax" },
+    );
+    cookieStore.set(
+      tokenConstant.refreshToken,
+      result.data[tokenConstant.refreshToken],
+      { httpOnly: true, maxAge: 60 * 60 * 24 * 7, sameSite: "lax" },
+    );
+  }
+
   return result;
 };
---
+++ src/constant/tokenConstant.ts
@@ -0,0 +1,6 @@
+export const tokenConstant = {
+  accessToken: "access_token",
+  refreshToken: "refresh_token",
+};
+
+Object.freeze(tokenConstant);
